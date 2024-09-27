4168d79b52aedb87586a25576522633d0afea7ad
André Velasco <83415790+KarotSystems@users.noreply.github.com>
2024-09-27 09:43:49 -0600

    Update
    Agregando más actividades

---
+++ Evaluación Parcial 2 (Parte 2)/Evaluación Parcial 2 (Parte 2)/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+string opcion;
+int factorial;
+Stack<int> pila = new Stack<int>();
+do
+{
+    Console.WriteLine("Factoriales");
+    Console.Write("Ingrese un número para calcular su factorial: ");
+    factorial = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("");
+    Console.WriteLine("Factoriales calculados:");
+    foreach (var resulta in pila)
+    {
+        Console.WriteLine(resulta);
+    }
+    int resultado = CalcularFactorialParcial(factorial, pila: pila);
+    Console.WriteLine("");
+    Console.WriteLine($"El factorial de {factorial} es: {resultado}");
+    pila.Push(resultado);
+    Console.WriteLine("");
+    Console.WriteLine("¿Desea calcular otro factorial? (S/Si para continuar):");
+    opcion = Console.ReadLine().ToUpper();
+    Console.Clear();
+} while (opcion == "S" || opcion == "SI");
+
+static int CalcularFactorialParcial(int num, Stack<int> pila)
+{
+    if (num <= 0)
+    {
+        return 1;
+    }
+    else
+    {
+        int resultado = 1;
+        for (int i = 1; i <= num; i++)
+        {
+            resultado *= i;
+            pila.Push(i);
+            int fact = pila.Pop();
+            Console.WriteLine(fact);
+        }
+        return resultado;
+    }
+}
---
+++ PilaRecursion/PilaRecursion/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine(EsBalanceado(Console.ReadLine(), new Stack<char>()) ? "Balanceado" : "No balanceado");
+bool EsBalanceado(string secuencia, Stack<char> pila, int indice = 0)
+{
+    if (indice == secuencia.Length) return pila.Count == 0;
+
+    char caracter = secuencia[indice];
+    if (caracter == '(' || caracter == '{' || caracter == '[') pila.Push(caracter);
+    else if (caracter == ')' && (pila.Count == 0 || pila.Pop() != '(') ||
+             caracter == '}' && (pila.Count == 0 || pila.Pop() != '{') ||
+             caracter == ']' && (pila.Count == 0 || pila.Pop() != '['))
+        return false;
+
+    return EsBalanceado(secuencia, pila, indice + 1);
+}
